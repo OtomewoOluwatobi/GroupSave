8a75694224a1d23a535600a9d6f63f2a538f01f2
Otomewo Oluwatobi. E <OtomewoOluwatobi@gmail.com>
2026-02-17 16:28:45 +0000

    fix: remove Registered event trigger from user registration transaction

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -97,9 +97,6 @@ class UserController extends Controller
                 return $user;
             });
 
-            // Trigger registered event (outside transaction)
-            event(new Registered($user));
-
             // Send onboarding notification (outside transaction)
             $user->notify(new UserOnboardingNotification($user));
 
