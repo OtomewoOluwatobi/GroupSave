2acc1f2a508e4354b67b005531a59f792d01f89a
Otomewo Oluwatobi. E <OtomewoOluwatobi@gmail.com>
2026-02-20 03:45:35 +0000

    refactor: add missing Notifiable trait import in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject, MustVerifyEmail
