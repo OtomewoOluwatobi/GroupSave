61a1289f3af2dd35de0fe15a5aaa4bd7ac02acd0
Otomewo Oluwatobi. E <OtomewoOluwatobi@gmail.com>
2026-02-24 02:28:27 +0000

    fix: change join request route from POST to GET for consistency with other routes

--- routes/api.php
+++ routes/api.php
@@ -77,7 +77,7 @@ Route::prefix('user')->middleware(['auth:api'])->group(function () {
         Route::get('/{id}/accept-invitation', [GroupController::class, 'acceptInvitation']);
 
         // Join request routes
-        Route::post('/{id}/send-join-request', [GroupController::class, 'sendJoinRequest']);
+        Route::get('/{id}/send-join-request', [GroupController::class, 'sendJoinRequest']);
         Route::get('/{id}/join-requests', [GroupController::class, 'getPendingJoinRequests']);
         Route::put('/{groupId}/join-requests/{requestId}/approve', [GroupController::class, 'approveJoinRequest']);
         Route::put('/{groupId}/join-requests/{requestId}/reject', [GroupController::class, 'rejectJoinRequest']);
